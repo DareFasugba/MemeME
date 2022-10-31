2856146d3640b2e5f40996eeb2d93a91df6ac891
Dare Fasugba <damilare2000@icloud.com>
2022-10-31 17:33:10 +1100

    Initial Commit

--- MemeMe/TextFieldDelegate.swift
+++ MemeMe/TextFieldDelegate.swift
@@ -35,6 +35,27 @@ class TextFieldDelegate: UIViewController, UITextFieldDelegate {
         }
         
     }
+    func subscribeToKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    func unsubscribeFromKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self)
+    }
+    @objc func keyboardWillShow(_ notification:Notification) {
+        if BottomTextField.isFirstResponder {
+            view.frame.origin.y = -getKeyboardHeight(notification)
+        }
+    }
+    @objc func keyboardWillHide(_ notification:Notification) {
+        view.frame.origin.y = 0
+    }
+    func getKeyboardHeight(_ notification:Notification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue // of CGRect
+        return keyboardSize.cgRectValue.height
+    }
+    
     
     
 }
--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
+class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate, UITextFieldDelegate {
     
     let imagePickerDelegate = ImagePickerDelegate()
     let textFieldDelegate = TextFieldDelegate()
@@ -17,10 +17,22 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavi
     @IBOutlet weak var ImagePickerView: UIImageView!
     @IBOutlet weak var NavigationItem: UIToolbar!
     @IBOutlet weak var LeftBarButton: UIBarButtonItem!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+    let textAttributes: [NSAttributedString.Key: Any] = [
+            NSAttributedString.Key.strokeColor: UIColor.black,
+            NSAttributedString.Key.foregroundColor: UIColor.white,
+            NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+            NSAttributedString.Key.strokeWidth:  -5.0
+        ]
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            TopTextField.delegate = self
+            BottomTextField.delegate = self
+            TopTextField.defaultTextAttributes = textAttributes
+            BottomTextField.defaultTextAttributes = textAttributes
+            TopTextField.textAlignment = .center
+            BottomTextField.textAlignment = .center
+        }
     
     @IBAction func pickAnImage(_ sender: Any) {
         let pickerController = UIImagePickerController()
@@ -47,24 +59,57 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavi
         return true
     }
     func subscribeToKeyboardNotifications() {
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
-    }
-    func unsubscribeToKeyboardNotifications() {
-        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
-    }
+            NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+            NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        }
+        func unsubscribeToKeyboardNotifications() {
+            NotificationCenter.default.removeObserver(self)
+        }
     @objc func keyboardWillShow(_ notification:Notification) {
-
-        view.frame.origin.y -= getKeyboardHeight(notification)
-    }
-
+            if BottomTextField.isFirstResponder {
+                view.frame.origin.y -= getKeyboardHeight(notification)
+            }
+        }
+        @objc func keyboardWillHide(_ notification:Notification) {
+            view.frame.origin.y = 0
+        }
     func getKeyboardHeight(_ notification:Notification) -> CGFloat {
-
         let userInfo = notification.userInfo
         let keyboardSize = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue // of CGRect
         return keyboardSize.cgRectValue.height
     }
-    
+   
+    var origImage: UIImageView!
+    var meme: Meme!
+    struct Meme {
+    var topTextField: String = ""
+    var bottomTextField: String = ""
+    var originalImage: UIImage!
+    var memedImage: UIImage!
+    } /* struct Meme s*/
+    //--------------------------
+    func generateMemedImage() -> UIImage {
+    //--------------------------
+    // TODO: Hide toolbar and navbar
+    // Render view to an image
+    UIGraphicsBeginImageContext(self.view.frame.size)
+    view.drawHierarchy(in: self.view.frame, afterScreenUpdates: true)
+    let memedImage: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+    UIGraphicsEndImageContext()
+    // TODO: Show toolbar and navbar
+    return memedImage
+    } /* generateMemedImage */
+    //--------------------------
+    func save() {
+    //--------------------------
+    // Create the meme
+    meme = Meme(topTextField: TopTextField.text!,
+    bottomTextField: BottomTextField.text!,
+    originalImage: ImagePickerView.image!,
+    memedImage: generateMemedImage())
+    } /* save */
     }
+    
         
 
 
